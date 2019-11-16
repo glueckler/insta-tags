0d8ec3a4e64e378c910ea2baf3e1ba666b71f9c9
Dean Glueckler <glueckler@gmail.com>
2019-11-16 11:00:07 -0800

    sup

--- js/app.js
+++ js/app.js
@@ -1,151 +1,228 @@
-const musicTags = [
-    "#ableton",
-    "#abletonlive",
-    "#abletontraining",
-    "#ableton101",
-    "#abletonlover",
-    "#electronicmusicculture",
-    "#electronicmusicfestival",
-    "#electronicmusicproducer ",
-    "#electronicmusician",
-    "#abletonlive",
-    "#sound",
-    "#boogie",
-    "#music",
-    "#bassmusic",
-    "#bassmusicproducer",
-    "#pksound",
-    "#halftime",
-    "#turtablism",
-    "#vibes",
-    "#musicfest",
-    "#moonmusic",
-    "#love",
-    "#beats",
-    "#freemusic",
-    "#freebass",
-    "#freebeats",
-    "#freebeat",
-    "#sendbeats",
-    "#beatsale",
-    "#draketypebeat",
-    "#upcomingartists",
-    "#djlife",
-    "#djset",
-    "#djlifestyle",
-    "#musicproducerlife",
-    "#musicproducergrind",
-    "#mixingengineer",
-    "#undergroundartist",
-    "#independentartist",
-    "#upandup",
-    "#seekjoy",
-    "#spotify",
-    "#producer",
-    "#dj",
-    "#studio",
-    "#beatmaker",
-    "#musicalfreedom",
-    "#musicproduction",
-    "#daintree",
-    "#daintreesound",
-    "#pressplay",
-    "#experimentalmusic",
-    "#ambience",
-    "#audioengineering",
-    "#audioengineer",
-    "#audioengineering",
-    "#studiovibes",
-    "#newbeats",
+const cbs = [
+  {
+    heada: 'Get Social',
+    firstTags: true,
+    tags: ['#sleepythecreator', '#sanctuaryluv', '#alians']
+  },
+  {
+    heada: 'locations',
+    firstTags: true,
+    tags: [
+      '#vancouver',
+      '#yvr',
+      '#bc',
+      '#alberta',
+      '#canada',
+      '#musicfest',
+      '#victoriamusicscene',
+      '#victoria',
+      '#yyj',
+      '#shambhala'
+    ]
+  },
+  {
+    heada: 'genres',
+    tags: [
+      '#halftime',
+      '#turtablism',
+      '#vibes',
+      '#beats',
+      '#VariousArtists',
+      '#BassMusic',
+      '#Abstract',
+      '#FutureBeats',
+      '#Dubstep',
+      '#Dnb',
+      '#Neuro',
+      '#leftfield'
+    ]
+  },
+  {
+    heada: 'production and performance',
+    tags: [
+      '#ableton',
+      '#abletonlive',
+      '#abletontraining',
+      '#creativespace',
+      '#ableton101',
+      '#abletonlover',
+      '#bassmusicproducer',
+      '#beats',
+      '#electronicmusicproducer ',
+      '#musicproducerlife',
+      '#mixengineer',
+      '#producer',
+      '#producerlife',
+      '#studio',
+      '#beatmaker',
+      '#musicalfreedom',
+      '#musicproduction',
+      '#daintree',
+      '#audioengineering',
+      '#studiovibes',
+      '#newbeats',
+      '#djlife',
+      '#djset',
+      '#dj'
+    ]
+  },
+  {
+    heada: 'musician',
+    tags: [
+      '#electronicmusicculture',
+      '#electronicmusician',
+      '#sound',
+      '#boogie',
+      '#music',
+      '#bassmusic',
+      '#pksound',
+      '#turtablism',
+      '#vibes',
+      '#moonmusic',
+      '#love',
+      '#beats',
+      '#freemusic',
+      '#freebeats',
+      '#upcomingartists',
+      '#djlifestyle',
+      '#musicproducerlife',
+      '#musicproducergrind',
+      '#mixingengineer',
+      '#undergroundartist',
+      '#independentartist',
+      '#upandup',
+      '#musicalfreedom',
+      '#daintree',
+      '#daintreesound',
+      '#pressplay',
+      '#experimentalmusic',
+      '#ambience'
+    ]
+  },
+  {
+    heada: 'Other Tags',
+    tags: [
+      '#Whooaahaawuw',
+      '#flow',
+      '#vibe',
+      '#imagination',
+      '#dance',
+      '#psychedelic',
+      '#freeimagination',
+      '#youknowwhoyouare',
+      '#creative',
+      '#upandup',
+      '#hippielife',
+      '#progress',
+      '#vibes',
+      '#vancouverartist',
+      '#hisimagination',
+      '#undergroundartist',
+      '#boogie',
+      '#galaxy',
+      '#sparkvitality',
+      '#healthylifestyle',
+      '#selfgrowth',
+      '#celebration',
+      '#party',
+      '#euphoria',
+      '#physicalhealth',
+      '#rave',
+      '#highonlife',
+      '#yogamusic',
+      '#play',
+      '#playfulness',
+      '#danceparty',
+      '#woofers',
+      '#mylife'
+    ]
+  }
 ];
 
-const generalTags = [
-    "#Whooaahaawuw",
-    "#bc",
-    "#alberta",
-    "#imagination",
-    "#dance",
-    "#psychedelic",
-    "#psychedelicart",
-    "#freeimagination",
-    "#youknowwhoyouare",
-    "#creative",
-    "#producerlife",
-    "#upandup",
-    "#hippielife",
-    "#progress",
-    "#vibes",
-    "#vancouverartist",
-    "#hisimagination",
-    "#undergroundartist",
-    "#boogie",
-    "#galaxy",
-    "#sparkvitality",
-    "#healthylifestyle",
-    "#selfgrowth",
-    "#celebration",
-    "#itsacelebration",
-    "#party",
-    "#euphoria",
-    "#physicalhealth",
-    "#partytime",
-    "#rave",
-    "#highonlife",
-    "#daybreaker",
-    "#yogamusic",
-    "#play",
-    "#playfulness",
-    "#happiness",
-    "#disinhibited ",
-    "#danceparty",
-    "#woofers",
-    "#mylife",
-    "#flow",
-    "#vibe"
-];
+//
 
-const visualArtTags = [
-    "#computerart",
-    "#digitalarts",
-    "#digitaldesign",
-    "#digitalabstractartist",
-    "#shapeart",
-    "#phoneart",
-    "#abstractart"
-];
+//
+
+const createElementFromHTML = htmlString => {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes
+  return div.firstChild;
+};
+
+// add checkboxes to page
+const $checkboxes = document.getElementById('checkboxes');
+
+const appendCheckbox = tag =>
+  $checkboxes.append(
+    createElementFromHTML(
+      `<div><input class="tag-cb" type="checkbox" name="${tag}" value="${tag}">${tag}</div>`
+    )
+  );
+
+cbs.forEach(({ heada, tags }) => {
+  $checkboxes.append(createElementFromHTML(`<h3>${heada}</h3>`));
+  tags.forEach(tag => appendCheckbox(tag));
+});
+
+//
+
+//
+
+// copying logic..
 
 const copyToClipboard = text => {
-    var textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    var result = document.execCommand('copy');
-    document.body.removeChild(textarea)
-    return result;
+  var textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  var result = document.execCommand('copy');
+  document.body.removeChild(textarea);
+  return result;
+};
+
+const shuffle = array => {
+  // this is probably really cool, from stack overflow
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
+const genCheckedTagsList = () => {
+  const firstTagList = [];
+  const tagList = [];
+  const MAX_TAGS = 27;
+  document.querySelectorAll('.tag-cb').forEach(({ checked, value }) => {
+    const firstOfAllList = cbs.find(({ firstTags }) => firstTags).tags;
+    if (checked && firstOfAllList.includes(value)) {
+      firstTagList.push(value);
+      return;
+    }
+    if (checked) {
+      tagList.push(value);
+    }
+  });
+  // prevent duplicates
+  return Array.from(new Set(firstTagList.concat(shuffle(tagList)).slice(0, MAX_TAGS + 1)));
 };
 
 const generateTagsStr = () => {
-    // concat the music and general tags together
-    const allTags = musicTags.concat(generalTags);
-
-    // build random array of length
-    const NUM_TAGS = 28;
-    const tags = ["#hisimagination", "#slzpyboi"];
-    const ranTag = () => allTags[Math.trunc(Math.random() * allTags.length)];
-    for (let i = 0; i <= NUM_TAGS; i++) {
-        let isNew = false;
-        let tag;
-        while (!isNew) {
-            tag = ranTag();
-            isNew = !tags.includes(tag)
-        }
-        tags.push(tag)
-    }
-    return `. \r\n. \r\n. \r\n. \r\n${tags.join(' . ')}`;
+  const checkedTags = genCheckedTagsList();
+  return `. \r\n. \r\n. \r\n. \r\n${checkedTags.join(' . ')}`;
 };
 
 const handleBBBClick = () => {
-    copyToClipboard(generateTagsStr())
+  copyToClipboard(generateTagsStr());
 };
 
-document.getElementById('bbb').addEventListener('click', handleBBBClick)
+document.getElementById('bbb').addEventListener('click', handleBBBClick);
